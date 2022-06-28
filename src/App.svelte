a56ecdd4d194582e40bf251ba9a1b5572e460b0f
Jerome Bacatan <yrombacatan@gmail.com>
2022-06-28 12:02:53 +0800

    minor changes

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,7 @@
   <div class="md:w-1/2 lg:w-3/5 xl:w-2/5 relative bg-dark_grayish_blue p-8 rounded-lg text-center">
     <p class="uppercase text-xs text-neon_green font-semibold tracking-extra-wide">Advice # {slip?.id ?? "Loading"}</p>
     <div class="my-8">
-      <p class="text-gray-300 font-semibold">{slip?.advice ?? "Loading"}</p>
+      <p class="text-gray-300 font-semibold">"{slip?.advice ?? "Loading"}"</p>
     </div>
     <div class="flex justify-center items-center mb-8">
       <span>
--- src/TailwindCss.svelte
+++ src/TailwindCss.svelte
@@ -1,10 +1,10 @@
 <style global>
+    @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap');
     @tailwind base;
     @tailwind utilities;
     @tailwind components;
 
     @layer base {
-        @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap');
         :root {
             --light-cyan: 193deg 38% 86%;
             --neon-green: 150deg 100% 66%;
